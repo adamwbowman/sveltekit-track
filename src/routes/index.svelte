ca25a0029064b0f91932295b1ebbc39c33b8d347
Adam Bowman <adamwbowman@gmail.com>
2022-01-27 23:34:12 -0600

    rough tag page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,27 @@
 
 <script>
-
 import { db } from '../lib/firebase'
-	import { collection, query, orderBy, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore"; 
+import { collection, query, orderBy, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore"; 
 
-	let expenses = [];
+let expenses = [];
 
-	// firestore entire get collection
-	const expensesCol = collection(db, 'expenses');
-		const queryAll = query(expensesCol,
-			orderBy("createdAt", "asc")
-		);
+// firestore entire get collection
+const expensesCol = collection(db, 'expenses');
+const queryAll = query(expensesCol,
+	orderBy("createdAt", "asc")
+);
 
-	// listener for collection reactivity
-	const listenCol = onSnapshot(queryAll, (querySnapshot) => {
-			expenses = querySnapshot.docs.map(doc => {
-				return { id: doc.id, ...doc.data() }
-			});
+// listener for collection reactivity
+const listenCol = onSnapshot(queryAll, (querySnapshot) => {
+	expenses = querySnapshot.docs.map(doc => {
+		return { id: doc.id, ...doc.data() }
+	});
 console.log(expenses);
-		});
-
-
-
+});
 </script>
-
 
 <h1>Home</h1>
 
-{#each expenses.filter(el => el.dayShort == "Wed") as expense}
-<p>{expense.location}</p>
+{#each expenses.filter(el => el.dayShort == "Mon") as expense}
+	<p><a href="/detail/{expense.tag}">{expense.tag}</a>{expense.location}</p>
 {/each}
