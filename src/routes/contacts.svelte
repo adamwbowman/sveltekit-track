a23b6c84c7aeb94947dbbae3334a09e1feb1fd89
Adam Bowman <adamwbowman@gmail.com>
2022-01-28 21:45:57 -0600

    array passing with slots

---
+++ src/routes/contacts.svelte
@@ -0,0 +1,15 @@
+<script>
+	const contacts = [
+		{ "name": "John", "age": 12 },
+		{ "name": "Jane", "age": 22 },
+		{ "name": "Mary", "age": 32 },
+		{ "name": "Joan", "age": 42 },
+		{ "name": "Jett", "age": 52 },
+		{ "name": "Jimm", "age": 6 },
+	];
+	console.log(contacts);
+</script>
+
+<div class="contact">
+	<slot {contacts} />
+</div>
