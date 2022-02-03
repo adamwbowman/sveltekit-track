435687ad8f52b90cdd6a2a20c4df4b015588bc94
Adam Bowman <adamwbowman@gmail.com>
2022-02-02 18:16:23 -0600

    wrapped up exp bt tag and by day

--- src/routes/analysis/index.svelte
+++ src/routes/analysis/index.svelte
@@ -28,16 +28,29 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col">
+		<div class="col-4">
+		<br /><b><u>Expenses by tag</u></b><br /><br />
 		{#each tags as tag}
 			<b>{tag} - count:{expenses.filter(el => (el.tag == tag)).length}</b><br />
 			{#each expenses.filter(el => el.tag == tag) as expense}
-				{expense.location} - {expense.tag} - {expense.amount}<br />
+				{expense.tag}:{expense.location} ${expense.amount}<br />
 			{/each}
-			total:{expenses.filter(el => (el.tag == tag)).reduce((accum, item) => accum + item.amount, 0)} <br />
-			avg: {(expenses.filter(el => (el.tag == tag)).reduce((accum, item) => accum + item.amount, 0)) / (expenses.filter(el => (el.tag == tag)).length)}
+			<i>total:{expenses.filter(el => (el.tag == tag)).reduce((accum, item) => accum + item.amount, 0)}, 
+			avg: {(expenses.filter(el => (el.tag == tag)).reduce((accum, item) => accum + item.amount, 0)) / (expenses.filter(el => (el.tag == tag)).length)}</i>
 			<br /><br />
 		{/each}
+		</div>
+		<div class="col-4">
+			<br /><b><u>Expenses by day</u></b><br /><br />
+			{#each days as day}
+				<b>{day} - count:{expenses.filter(el => (el.day == day)).length}</b><br />
+				{#each expenses.filter(el => el.day == day) as expense}
+					{expense.location} - {expense.tag} - {expense.amount}<br />
+				{/each}
+				<i>total:{expenses.filter(el => (el.day == day)).reduce((accum, item) => accum + item.amount, 0)},
+				avg: {(expenses.filter(el => (el.day == day)).reduce((accum, item) => accum + item.amount, 0)) / (expenses.filter(el => (el.day == day)).length)}</i>
+				<br /><br />
+			{/each}
 		</div>
 	</div>
 </div>
